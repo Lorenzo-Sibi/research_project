2031ab800ba87a04d958610685f15d5a41aa34e2
Lorenzo-Sibi <lorenzosibi2001@gmail.com>
2023-11-24 12:52:23 +0100

    added comand in main.py for log of all tensors of  all models

--- main.py
+++ main.py
@@ -1,22 +1,77 @@
 import argparse
 import os
+import sys
 
 from src import preprocess
 from src import tensor_extraction
-from src import utils
 
-MODELS_DICT = {
-    "hific": ["hific-lo", "hific-mi", "hific-hi"],
-    "ms2020": ["ms2020-cc10-mse-[1-10]", "ms2020-cc8-msssim-[1-9]"],
-    "mbt218": ["mbt2018-mean-mse-[1-8]", "mbt2018-mean-msssim-[1-8]"],
-    "bmshj2018": ["bmshj2018-factorized-mse-[1-8]", "bmshj2018-factorized-msssim-[1-8]", "bmshj2018-hyperprior-mse-[1-8]", "bmshj2018-hyperprior-msssim-[1-8]"],
-    "b2018": ["b2018-leaky_relu-128-[1-4]", "b2018-leaky_relu-192-[1-4]", "b2018-gdn-128-[1-4]", "b2018-gdn-192-[1-4]"]
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "compression-master/models"))
+import tfci
+
+
+MODELS_DICT = MODELS_DICT = {
+    "hific": {
+        "variants": ["hific-lo", "hific-mi", "hific-hi"]
+    },
+    "ms2020": {
+        "cc10": [f"ms2020-cc10-mse-{i}" for i in range(1, 11)],
+        "cc8": [f"ms2020-cc8-msssim-{i}" for i in range(1, 10)]
+    },
+    "mbt218": {
+        "mean": [f"mbt2018-mean-mse-{i}" for i in range(1, 9)],
+        "mean-msssim": [f"mbt2018-mean-msssim-{i}" for i in range(1, 9)]
+    },
+    "bmshj2018": {
+        "factorized-mse": [f"bmshj2018-factorized-mse-{i}" for i in range(1, 9)],
+        "factorized-msssim": [f"bmshj2018-factorized-msssim-{i}" for i in range(1, 9)],
+        "hyperprior-mse": [f"bmshj2018-hyperprior-mse-{i}" for i in range(1, 9)],
+        "hyperprior-msssim": [f"bmshj2018-hyperprior-msssim-{i}" for i in range(1, 9)]
+    },
+    "b2018": {
+        "leaky_relu-128": [f"b2018-leaky_relu-128-{i}" for i in range(1, 5)],
+        "leaky_relu-192": [f"b2018-leaky_relu-192-{i}" for i in range(1, 5)],
+        "gdn-128": [f"b2018-gdn-128-{i}" for i in range(1, 5)],
+        "gdn-192": [f"b2018-gdn-192-{i}" for i in range(1, 5)]
+    }
 }
-
-MODELS_LIST = sum(MODELS_DICT.values(), [])
 
 TENSOR_NAMES = ["hyperprior/entropy_model/conditional_entropy_model_3/add:0"]
 MODEL_NAME = "hific-lo"
+
+def list_tensors(model):
+  """Lists all internal tensors of a given model."""
+  def get_names_dtypes_shapes(function):
+    for op in function.graph.get_operations():
+      for tensor in op.outputs:
+        yield tensor.name, tensor.dtype.name, tensor.shape
+
+  sender = tfci.instantiate_model_signature(model, "sender")
+  tensors = sorted(get_names_dtypes_shapes(sender))
+  log = "Sender-side tensors:\n"
+  for name, dtype, shape in tensors:
+    log += f"{name} (dtype={dtype}, shape={shape})\n"
+  log += "\n"
+
+  receiver = tfci.instantiate_model_signature(model, "receiver")
+  tensors = sorted(get_names_dtypes_shapes(receiver))
+  log += "Receiver-side tensors:\n"
+  for name, dtype, shape in tensors:
+    log += f"{name} (dtype={dtype}, shape={shape})\n"
+  return log
+
+def tensors_log(logdir='tensors_logs'):
+    for model_class in MODELS_DICT:
+        for variant in MODELS_DICT[model_class]:
+            for model in MODELS_DICT[model_class][variant]:
+                path = os.path.join(logdir, model_class, variant)
+                filename = os.path.join(path, model + "tensors" + ".txt")
+                if not os.path.exists(path):
+                    os.makedirs(path)
+                with open(filename, "w") as f:
+                   f.write(list_tensors(model))
+
+                
+
 def main(args):
     if args.command == "compress":
         if args.one_image:
@@ -44,6 +99,9 @@ def main(args):
     
     elif args.command == "tensors":
         tensor_extraction.list_tensors(args.model)
+
+    elif args.command == "tensors-all":
+        tensors_log()
 
     elif args.command == "dump":
         if args.all_images:
@@ -106,7 +164,6 @@ def parse_args():
     )
     dump_cmd.add_argument(
         "model",
-        choices=MODELS_LIST,
         help="Specify the name of the model"
     )
     dump_cmd.add_argument(
@@ -118,13 +175,18 @@ def parse_args():
 
     # 'tensors' subcommand.
     tensors_cmd = subparser.add_parser(
+      "tensors-all",
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+      description="Lists names of internal tensors of a given model.")
+
+    # 'tensors' subcommand.
+    tensors_cmd = subparser.add_parser(
       "tensors",
       formatter_class=argparse.ArgumentDefaultsHelpFormatter,
       description="Lists names of internal tensors of a given model.")
     
     tensors_cmd.add_argument(
         "model",
-        choices=MODELS_LIST,
         help="Unique model identifier. See 'models' command for options.")
 
     # 'crop' subcommand.
